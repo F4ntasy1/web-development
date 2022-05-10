0b94e4d812b0f9cd49dedca607f7ec3770b4734b
F4ntasy1 <dmitriypet03@bk.ru>
2022-05-10 20:58:10 +0300

    lw12

--- lw12/mysql.sql
+++ lw12/mysql.sql
@@ -15,13 +15,13 @@ USE Universe;
 CREATE TABLE Facoulty
 (
     Id INT AUTO_INCREMENT PRIMARY KEY,
-    FacoultyName VARCHAR(50) NOT NULL
+    FacoultyName VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Groups
 (
     Id INT AUTO_INCREMENT PRIMARY KEY,
-    GroupName VARCHAR(50) NOT NULL,
+    GroupName VARCHAR(255) NOT NULL,
     GroupNumber INT NOT NULL,
     FacoultyId INT NOT NULL,
     FOREIGN KEY (FacoultyId) REFERENCES Facoulty (Id)
@@ -30,7 +30,8 @@ CREATE TABLE Groups
 CREATE TABLE Student 
 (
     Id INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(50) NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    FamilyName VARCHAR(255) NOT NULL,
     Age INT NOT NULL,
     GroupId INT NOT NULL,
     FacoultyId INT NOT NULL,
@@ -55,59 +56,59 @@ INSERT INTO Groups (GroupName, GroupNumber, FacoultyId) VALUES ('РТ', 11, 3);
 INSERT INTO Groups (GroupName, GroupNumber, FacoultyId) VALUES ('РТ', 12, 3);
 INSERT INTO Groups (GroupName, GroupNumber, FacoultyId) VALUES ('РТ', 13, 3);
 
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 19, 1, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Михаил', 20, 1, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Павел', 20, 1, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Анна', 19, 1, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дарья', 19, 1, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 'Македонский', 19, 1, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Михаил', 'Грачев', 20, 1, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Павел', 'Дуров', 20, 1, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Анна', 'Макаренко', 19, 1, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дарья', 'Пирогова', 19, 1, 1);
 
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 19, 2, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Михаил', 20, 2, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Павел', 20, 2, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Анна', 19, 2, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дарья', 19, 2, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 'Македонский', 19, 2, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Михаил', 'Грачев', 20, 2, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Павел', 'Дуров', 20, 2, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Анна', 'Макаренко', 19, 2, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дарья', 'Пирогова', 19, 2, 1);
 
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 19, 3, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Михаил', 20, 3, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Павел', 20, 3, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Анна', 19, 3, 1);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дарья', 19, 3, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 'Македонский', 19, 3, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Михаил', 'Грачев', 20, 3, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Павел', 'Дуров', 20, 3, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Анна', 'Макаренко', 19, 3, 1);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дарья', 'Пирогова', 19, 3, 1);
 
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 19, 4, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Михаил', 20, 4, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Павел', 20, 4, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Анна', 19, 4, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дарья', 19, 4, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 'Македонский', 19, 4, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Михаил', 'Грачев', 20, 4, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Павел', 'Дуров', 20, 4, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Анна', 'Макаренко', 19, 4, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дарья', 'Пирогова', 19, 4, 2);
 
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 19, 5, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Михаил', 20, 5, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Павел', 20, 5, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Анна', 19, 5, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дарья', 19, 5, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 'Македонский', 19, 5, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Михаил', 'Грачев', 20, 5, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Павел', 'Дуров', 20, 5, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Анна', 'Макаренко', 19, 5, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дарья', 'Пирогова', 19, 5, 2);
 
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 19, 6, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Михаил', 20, 6, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Павел', 20, 6, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Анна', 19, 6, 2);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дарья', 19, 6, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 'Македонский', 19, 6, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Михаил', 'Грачев', 20, 6, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Павел', 'Дуров', 20, 6, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Анна', 'Макаренко', 19, 6, 2);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дарья', 'Пирогова', 19, 6, 2);
 
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 19, 7, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Михаил', 20, 7, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Павел', 20, 7, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Анна', 19, 7, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дарья', 19, 7, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 'Македонский', 19, 7, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Михаил', 'Грачев', 20, 7, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Павел', 'Дуров', 20, 7, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Анна', 'Макаренко', 19, 7, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дарья', 'Пирогова', 19, 7, 3);
 
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 19, 8, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Михаил', 20, 8, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Павел', 20, 8, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Анна', 19, 8, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дарья', 19, 8, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 'Македонский', 19, 8, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Михаил', 'Грачев', 20, 8, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Павел', 'Дуров', 20, 8, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Анна', 'Макаренко', 19, 8, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дарья', 'Пирогова', 19, 8, 3);
 
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 19, 9, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Михаил', 20, 9, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Павел', 20, 9, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Анна', 19, 9, 3);
-INSERT INTO Student (Name, Age, GroupId, FacoultyId) VALUES ('Дарья', 19, 9, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дмитрий', 'Македонский', 19, 9, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Михаил', 'Грачев', 20, 9, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Павел', 'Дуров', 20, 9, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Анна', 'Макаренко', 19, 9, 3);
+INSERT INTO Student (Name, FamilyName, Age, GroupId, FacoultyId) VALUES ('Дарья', 'Пирогова', 19, 9, 3);
 
 
 
@@ -117,7 +118,8 @@ SELECT * FROM Student WHERE Age = 19;
 
 SELECT 
     Student.Id, 
-    Student.Name, 
+    Student.Name,
+    Student.FamilyName, 
     Student.Age 
 FROM 
     Student 
@@ -132,7 +134,8 @@ WHERE
 
 SELECT 
     Student.Id, 
-    Student.Name, 
+    Student.Name,
+    Student.FamilyName, 
     Student.Age 
 FROM 
     Student 
